012573bd447db732c7c5788892860b6822209e83
Alisbur <mail-4-eugen@yandex.ru>
2023-02-14 19:32:00 +0300

    Подтянул изображения из своего ПР4

--- scripts/index.js
+++ scripts/index.js
@@ -5,28 +5,28 @@ setTimeout(() => {
 
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Кейптаун, ЮАР',
+    link: './images/cape-town.jpg'
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Макао, Китай',
+    link: './images/macau.jpg'
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Гавайи, США',
+    link: './images/havaii.jpg'
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Флорианополис, Бразилия',
+    link: './images/florianopolis.jpg'
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Тенерифе, Канарские острова',
+    link: './images/tenerife.jpg'
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Санкт-Петербург, Россия',
+    link: './images/saint-petersburg.jpg'
   }
 ]; 
 
